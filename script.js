8ec67c88b53cc1abe51f52fccfbd997f7859d032
Jonathan Pereira <john.rec@hotmail.com>
2024-06-11 11:22:32 -0300

    ajustando operador OR(finalizado)

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
 
 
 function playRockPaperScissor (player1, player2) {
-
-    if (player1 =="Pedra" && player2 == "Tesoura"){
+    if (((player1 =="Pedra") && (player2 == "Tesoura")) || ((player1 =="Tesoura") && (player2 =="Papel")) || ((player1 =="Papel") && (player2 =="Pedra"))) {
         return "Jogador 1 venceu!"
-    }if(player1 =="Tesoura" && player2 =="Papel"){
-        return "Jogador 1 venceu!"
-    }if(player1 =="Papel" && player2 =="Pedra") {
-        return "Jogador 1 venceu!"
-    }if(player2 =="Pedra" && player1 == "Tesoura"){
+    }else if(((player2 =="Pedra") && (player1 == "Tesoura"))|| ((player2 =="Tesoura") && (player1 =="Papel")) || ((player2 =="Papel") && (player1 =="Pedra"))) {
         return "Jogador 2 venceu!"
-    }if(player2 =="Tesoura" && player1 =="Papel"){
-        return "Jogador 2 venceu!"
-    }if(player2 =="Papel" && player1 =="Pedra") {
-        return "Jogador 2 venceu!"
-    }if(player1 =="Papel" && player2 =="Papel"){
-        return "Empate!"
-    }if(player1 =="Tesoura" && player2 =="Tesoura") {
-        return "Empate!"
-    }if(player1 =="Pedra" && player2 =="Pedra") {
+    }else if(((player1 =="Papel") && (player2 =="Papel")) || ((player1 =="Tesoura") && (player2 =="Tesoura")) || ((player1 =="Pedra") && (player2 =="Pedra"))) {
         return "Empate!"
     }
-
+        
 }
+    
+
+
 
